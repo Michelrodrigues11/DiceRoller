af184d8a56d02d3c42b02804b9c028cb74790083
Michel <henriquemichel10@gmail.com>
2023-09-18 22:51:58 -0300

    foi

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DiceRoller;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DiceRoller;
 
 public partial class MainPage : ContentPage
 {
@@ -10,23 +12,23 @@ public partial class MainPage : ContentPage
 
     
 
-    protected class Dado
+    protected class Dado //Classe
     {
 
-        public Dado()
+        private int NumerodeLados; //Atributo
+
+        public Dado() //Construtor
         { }
 
-        public Dado(int numerodeLados)
+        public Dado(int numerodeLados) //Metodo
         {
             NumerodeLados = numerodeLados;
         }
 
-        private int NumerodeLados;
-
         public int RollDice()
         {
-            int ramdom =  new Random().Next(1, NumerodeLados + 1); ;
-            return ramdom;
+            int random =  new Random().Next(1, NumerodeLados + 1); ;
+            return random;
         }
         
      
@@ -35,13 +37,12 @@ public partial class MainPage : ContentPage
     private void OnButtonClicked(object sender, EventArgs e)
     {
 
-        int NumerodeLados = (Int32)SeletorDosLados.SelectedItem;
-        Dado dado= new Dado(NumerodeLados);
-        numberOutput.Text = dado.RollDice().ToString();
+        int NumerodeLados = (Int32)SeletorDosLados.SelectedItem; //Ler o numero que o usuario selecionou no Picker.
+        Dado dado = new Dado(NumerodeLados);//Instancia um novo dado com N .Onde N é o número selecionado. // Chamar o método para sortear o número.
+        numberOutput.Text = dado.RollDice().ToString();//Mostrar o número no label.
 
 
-
-
+    }
 
 
         /*int min = 1;
@@ -54,9 +55,17 @@ public partial class MainPage : ContentPage
         int numeroGerado = random.Next(min, max);
         numberOutput.Text = numeroGerado.ToString();*/
 
-        /*código referencia para adicionar tema
-          
-        AppTheme currentTheme = Application.Current.RequestedTheme;
+
+        /*código referencia para adicionar tema*/
+
+       public void OnChangeOption(object sender, EventArgs e)
+        {
+            int NumerodeLados = (int)SeletorDosLados.SelectedItem;
+            Image image = new Image { Source = $"dice_d (NumerodeLados).png" };
+            diceImage.Source = image.Source;
+        }
+
+      /*  AppTheme currentTheme = Application.Current.RequestedTheme;
         if(currentTheme.ToString() != "Unspecified")
         { 
             diceImage.Source = ImageSource.FromFile($"{currentTheme}_dice_d{NumerodeLados}.png"); 
@@ -68,4 +77,4 @@ public partial class MainPage : ContentPage
 
 
     }
-}
+
